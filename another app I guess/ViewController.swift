e098ead13b8d4f97c4f87d99dde5f362514cb6ed
Yael_Amaya <yiap2610@gmail.com>
2017-10-10 11:35:43 -0600

    Initial Commit

---
+++ another app I guess/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  another app I guess
+//
+//  Created by Amaya Penunuri, Yael on 10/10/17.
+//  Copyright © 2017 Amaya Penunuri, Yael. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
